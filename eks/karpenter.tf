6c74e42cf23436aab14803b45323d7045f0814d2
Jared Thivener <jaredthivener@gmail.com>
2025-06-27 20:01:41 -0400

    bumped karpenter to v1.5.1 and updated apiVersion

--- eks/karpenter.tf
+++ eks/karpenter.tf
@@ -14,7 +14,7 @@ resource "helm_release" "karpenter" {
   name             = "karpenter"
   repository       = "https://charts.karpenter.sh"
   chart            = "karpenter"
-  version          = "1.5.0"
+  version          = "1.5.1"
   namespace        = "karpenter"
   create_namespace = true
 
